2862ca59f3d6f11af8388b1cee012f0376e04ee2
agusrypstra <agusrypstra@gmail.com>
2025-04-21 21:18:06 -0300

    interface renovada con la api

--- src/presentation/interfaces/SolicitudRegistroLocalInterface.tsx
+++ src/presentation/interfaces/SolicitudRegistroLocalInterface.tsx
@@ -1,23 +1,24 @@
 interface SolicitudRegistroLocal {
-    usuarioId: number;
-    categoriaId: number;
-    nombre: string;
-    provincia: string;
-    localidad: string;
-    direccion: string;
-    telefonoLlamadas: string;
-    telefonoWhatsapp: string;
-    descripcion: string;
-    diasAtencionDesde: string;
-    diasAtencionHasta: string;
-    horarioAtencionDesde: string;
-    horarioAtencionHasta: string;
-    es24Horas: boolean;
-    ubicacionGoogleMaps: string;
-    linkInstagram: string;
-    linkFacebook: string;
-    linkPaginaWeb: string;
-    fotoPerfil: string;
-    fotoBanner: string;
-  }
-  
+  id: number;
+  nombre: string;
+  direccion: string;
+  provincia: string;
+  localidad: string;
+  telefonoLlamadas: string;
+  telefonoWhatsapp: string;
+  ubicacionGoogleMaps: string;
+  descripcion: string;
+  diasAtencionDesde: string;
+  diasAtencionHasta: string;
+  horarioAtencionDesde: string;
+  horarioAtencionHasta: string;
+  es24Horas: boolean;
+  linkInstagram: string;
+  linkFacebook: string;
+  linkPaginaWeb: string;
+  fotoPerfil: string;
+  fotoBanner: string;
+  estado: string;
+  categoriaNombre: string;
+  usuarioNombre: string;
+}
